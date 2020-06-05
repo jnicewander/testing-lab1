f68252019a80392f176860624231c86ba5d8c81e
Justin Nicewander <jnicewander@live.com>
2020-06-04 22:02:13 -0400

    Initial commit.

--- tests/changehandler.test.js
+++ tests/changehandler.test.js
@@ -1,1 +1,19 @@
 let { ChangeHandler } = require("../src/changehandler");
+
+
+describe('Constructor Is Set Correctly', function() {
+    test('Amount Due Is Set', function() {
+        const change = new ChangeHandler(80);
+        expect(change.amountDue).toEqual(80);
+    });
+    test('Cash Tendered Is Set To Zero', function() {
+        const change = new ChangeHandler(80);
+        expect(change.cashTendered).toEqual(0);
+    });
+});
+
+describe('Inserting Various Coins Shows Correct Amounts', function() {
+    test('Inserting a quarter adds 25', function() {
+        
+    });
+});
