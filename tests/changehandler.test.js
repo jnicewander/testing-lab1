1e19d0259111e99fb47b3bcd95533f9d92eea899
J.Nicewander <jnicewander@live.com>
2020-06-05 11:31:56 -0400

    Finished tests for 1 through 3 of required test cases.

--- tests/changehandler.test.js
+++ tests/changehandler.test.js
@@ -14,6 +14,48 @@ describe('Constructor Is Set Correctly', function() {
 
 describe('Inserting Various Coins Shows Correct Amounts', function() {
     test('Inserting a quarter adds 25', function() {
-        
+        const change = new ChangeHandler(0);
+        change.insertCoin('quarter');
+        expect(change.cashTendered).toEqual(25);
+    });
+    test('Inserting a dime adds 10', function() {
+        const change = new ChangeHandler(0);
+        change.insertCoin('dime');
+        expect(change.cashTendered).toEqual(10);
+    });
+    test('Inserting a nickel adds 5', function() {
+        const change = new ChangeHandler(0);
+        change.insertCoin('nickel');
+        expect(change.cashTendered).toEqual(5);
+    });
+    test('Inserting a penny adds 1', function() {
+        const change = new ChangeHandler(0);
+        change.insertCoin('penny');
+        expect(change.cashTendered).toEqual(1);
+    });
+    test('Calling the function multiple times continues adding', function() {
+        const change = new ChangeHandler(0);
+        change.insertCoin('quarter');
+        change.insertCoin('penny');
+        change.insertCoin('dime');
+        expect(change.cashTendered).toEqual(36)
+    });
+});
+
+describe('isPaymentSufficient returning the correct boolean', function() {
+    test('Returns true if cashTendered more than amountDue', function() {
+        const change = new ChangeHandler(10);
+        change.insertCoin('quarter');
+        expect(change.isPaymentSufficient()).toBeTruthy();
+    });
+    test('Returns false if cashTendered less than amountDue', function() {
+        const change = new ChangeHandler(35);
+        change.insertCoin('quarter');
+        expect(change.isPaymentSufficient()).toBeFalsy();
+    });
+    test('Returns true if cashTendered equal to amountDue', function() {
+        const change = new ChangeHandler(25);
+        change.insertCoin('quarter');
+        expect(change.isPaymentSufficient()).toBeTruthy();
     });
 });
